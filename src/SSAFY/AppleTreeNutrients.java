30a757ab01d6b2172e389626c1febb3367de4eac
xabin <binine2@gmail.com>
2025-01-22 14:46:25 +0900

    SSFAY GIT 사과나무 영양분

---
+++ src/SSAFY/AppleTreeNutrients.java
@@ -0,0 +1,41 @@
+package SSAFY;
+
+import java.util.Scanner;
+
+public class AppleTreeNutrients {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[][] graph = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = sc.nextInt();
+            }
+        }
+
+        int maxNutrients = 0;
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int temp = graph[i][j];
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                        break;
+                    }
+                    temp += graph[nx][ny];
+                }
+                maxNutrients = Math.max(maxNutrients, temp);
+            }
+        }
+        System.out.println(maxNutrients);
+    }
+}
