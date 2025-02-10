b2968662eadc091e3b4fadaaf70aa888581f9d5f
xabin <binine2@gmail.com>
2025-02-10 11:33:45 +0900

    algo : 평탄화

---
+++ src/SSAFY/Flatten.java
@@ -0,0 +1,48 @@
+package SSAFY;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Flatten {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int t = 1; t <= 10; t++) {
+            int dumpCount = sc.nextInt();
+            sc.nextLine();
+
+            List<Integer> arr = Stream.of(sc.nextLine().split(" ")).
+                    map(Integer::parseInt).
+                    collect(Collectors.toList());
+
+            for (int i = 0; i < dumpCount; i++) {
+                int minIndex = 0;
+                int maxIndex = 0;
+
+                for (int j = 0; j < arr.size(); j++) {
+                    if (arr.get(j) < arr.get(minIndex)) {
+                        minIndex = j;
+                    }
+
+                    if (arr.get(j) > arr.get(maxIndex)) {
+                        maxIndex = j;
+                    }
+                }
+
+                arr.set(minIndex, arr.get(minIndex) + 1);
+                arr.set(maxIndex, arr.get(maxIndex) - 1);
+
+                if (arr.get(maxIndex) - arr.get(minIndex) <= 1) {
+                    break;
+                }
+            }
+
+            int result = Collections.max(arr) - Collections.min(arr);
+
+            System.out.println("#" + t + " " + result);
+        }
+    }
+}
