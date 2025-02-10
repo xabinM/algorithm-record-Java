e43a74d43008af247ffcd190caff83ae21bf086a
xabin <binine2@gmail.com>
2025-02-10 16:33:49 +0900

    algo:  전기 버스

--- src/SSAFY/ElectricBus.java
+++ src/SSAFY/ElectricBus.java
@@ -10,23 +10,41 @@ public class ElectricBus {
 
         int T = sc.nextInt();
 
-        for (int i = 0; i < T; i++) {
+        for (int t = 1; t <= T; t++) {
             int K = sc.nextInt();
             int N = sc.nextInt();
             int M = sc.nextInt();
-            sc.nextLine();
 
             List<Integer> busStop = new ArrayList<>();
-            for (int j = 0; j < M ; j++) {
+            for (int i = 0; i < M; i++) {
                 busStop.add(sc.nextInt());
             }
-            sc.nextLine();
 
+            int idx = 0;
+            int cnt = 0;
+            boolean flag = true;
 
-
-
-
-
+            Outer : while (idx < N) {
+                int temp = idx;
+                for (int j = K; j >= 1; j--) {
+                    if ((idx + K) >= N) {
+                        break Outer;
+                    }
+                    if (busStop.contains(idx + j)) {
+                        idx += j;
+                        cnt += 1;
+                        break;
+                    }
+                }
+                if (temp == idx){
+                    System.out.println("#" + t + " " + 0);
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                System.out.println("#" + t + " " + cnt);
+            }
         }
     }
 }
