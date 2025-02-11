c8271c83bbee4804be90a5a8b6f0a2c92ae9cbfa
xabin <binine2@gmail.com>
2025-02-11 09:50:12 +0900

    algo : 행열 대각선 중 최대값 찾기 최종

--- src/SSAFY/Sum.java
+++ src/SSAFY/Sum.java
@@ -4,10 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class Sum {
     public static void main(String[] args) throws IOException {
@@ -15,38 +15,41 @@ public class Sum {
 
         for (int i = 0; i < 10; i++) {
             int T = Integer.parseInt(br.readLine());
-            List<List<Integer>> graph = new ArrayList<>();
-            for (int j = 0; j < 100; j++){
-                List<Integer> temp = Stream.of(br.readLine().split(" ")).
-                        map(Integer::parseInt).
-                        collect(Collectors.toList());
-                graph.add(temp);
+
+            int[][] graph = new int[100][100];
+            for (int r = 0; r < 100; r++) {
+                String[] temp = br.readLine().split(" ");
+                for (int c = 0; c < 100; c++) {
+                    graph[r][c] = Integer.parseInt(temp[c]);
+                }
             }
+
 
             int leftCross = 0;
             int rightCross = 0;
-
             List<Integer> result = new ArrayList<>();
-            for (int x = 0; x < 100; x++) {
-                int row = graph.get(x).stream().mapToInt(Integer::intValue).sum();
-                result.add(row);
 
+            for (int x = 0; x < 100; x++) {
+                result.add(Arrays.stream(graph[x]).sum());
+                int col = 0;
                 for (int y = 0; y < 100; y++) {
-                    if (y == 0) {
-                        result.add(graph.get(x).get(y));
-                    }
+                    col += graph[y][x];
+
                     if (x == y) {
-                        leftCross += graph.get(x).get(y);
+                        leftCross += graph[x][y];
                     }
                     if (x + y == 99) {
-                        rightCross += graph.get(x).get(y);
+                        rightCross += graph[x][y];
                     }
                 }
+                result.add(col);
             }
 
             result.add(leftCross);
             result.add(rightCross);
-            System.out.println("#" + T + " " + Collections.max(result));
+            int maxResult = Collections.max(result);
+
+            System.out.println("#" + T + " " + maxResult);
         }
     }
 }
