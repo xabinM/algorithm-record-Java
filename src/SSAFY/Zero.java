01cb2aff453d5d26f7c91bf55993f9aa1c8f843c
xabin <binine2@gmail.com>
2025-02-13 14:34:44 +0900

    algo : 영수증

---
+++ src/SSAFY/Zero.java
@@ -0,0 +1,30 @@
+package SSAFY;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Zero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tc = sc.nextInt();
+
+        for (int t = 1; t <= tc; t++) {
+            int K = sc.nextInt();
+
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < K; i++) {
+                int price = sc.nextInt();
+
+                if (!stack.isEmpty() && price == 0) {
+                    stack.pop();
+                } else if (price != 0) {
+                    stack.push(price);
+                }
+            }
+            int result = stack.stream().mapToInt(Integer::intValue).sum();
+
+            System.out.println("#" + t + " " + result);
+        }
+    }
+}
