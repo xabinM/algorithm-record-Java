182deb3e143db6f8efaaaaef76a537f83deab4a9
xabin <binine2@gmail.com>
2025-02-13 13:19:35 +0900

    algo : 문자열 비교하기

---
+++ src/SSAFY/CompareString.java
@@ -0,0 +1,24 @@
+package SSAFY;
+
+import java.util.Scanner;
+
+public class CompareString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int t = 1; t <= T; t++) {
+            String str1 = sc.nextLine();
+            String str2 = sc.nextLine();
+
+            if (str2.length() == str2.replace(str1, "").length()) {
+                System.out.println("#" + t + " " + 0);
+            } else {
+                System.out.println("#" + t + " " + 1);
+            }
+
+        }
+    }
+}
