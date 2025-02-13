7ae836421aa1c77522b60fed9615009a7e7550c8
Min <binine2@gmail.com>
2025-02-13 19:53:17 +0900

    algo : 오목 판정

--- src/SSAFY/이차원배열/Concave.java
+++ src/SSAFY/이차원배열/Concave.java
@@ -21,23 +21,35 @@ public class Concave {
             int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
             int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
 
-            for (int i = 0; i < N; i++) {
+            boolean flag = false;
+
+            Outer : for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
 
                     if (graph[i][j].equals("o")){
                         for (int k = 0; k < 8; k++) {
-                            int nx = i + dx[k];
-                            int ny = j + dy[k];
-                            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                                if (graph[nx][ny].equals("o")) {
-
+                            int cnt = 0;
+                            for (int p = 1; p < 5; p++) {
+                                int nx = i + dx[k] * p;
+                                int ny = j + dy[k] * p;
+                                if (nx < 0 || nx >= N || ny < 0 || ny >= N || !graph[nx][ny].equals("o")) {
+                                    break;
                                 }
+                                cnt++;
+                            }
+                            if (cnt == 4) {
+                                flag = true;
+                                break Outer;
                             }
                         }
                     }
                 }
             }
-
+            if (flag) {
+                System.out.println("#" + t + " " + "YES");
+            } else {
+                System.out.println("#" + t + " " + "NO");
+            }
         }
     }
 }
