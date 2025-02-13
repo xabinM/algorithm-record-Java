770159c9f3531475633ba8939a63e80c81eef453
xabin <binine2@gmail.com>
2025-02-13 15:18:57 +0900

    algo : 쇠막대 자르기

---
+++ src/SSAFY/CuttingStealStick.java
@@ -0,0 +1,38 @@
+package SSAFY;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CuttingStealStick {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int t = 1; t <= T; t++) {
+            String[] brackets = sc.nextLine().split("");
+            Stack<String> stack = new Stack<>();
+
+            int result = 0;
+            String pre = "";
+
+            for (String str : brackets) {
+                if (str.equals("(")) {
+                    stack.push(str);
+                    pre = str;
+                } else {
+                    stack.pop();
+                    if (pre.equals(")")) {
+                        result += 1;
+                        pre = str;
+                    } else {
+                        result += stack.size();
+                        pre = str;
+                    }
+                }
+            }
+            System.out.println("#" + t + " " + result);
+        }
+    }
+}
