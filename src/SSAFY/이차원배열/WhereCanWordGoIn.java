c2ac8bf4be1a948972b847ce8f4ad80293e8b083
xabin <binine2@gmail.com>
2025-02-14 10:07:11 +0900

    algo : 어디에 단ã어가 들어갈 수 있을까? 2차원배열 가로 세로 분리

--- src/SSAFY/이차원배열/WhereCanWordGoIn.java
+++ src/SSAFY/이차원배열/WhereCanWordGoIn.java
@@ -19,44 +19,44 @@ public class WhereCanWordGoIn {
                 graph[i] =  sc.nextLine().split(" ");
             }
 
-            int[] dx = {-1, 1, 0, 0};
-            int[] dy = {0, 0, -1, 1};
-
             int result = 0;
+
             for (int x = 0; x < N; x++) {
+                int cnt = 0;
                 for (int y = 0; y < N; y++) {
-
-                    if (graph[x][y].equals("1")){
-                        for (int k = 0; k < 4; k++) {
-                            int cnt = 0;
-                            for (int p = 1; p < N; p++){
-                                int nx = x + dx[k] * p;
-                                int ny = y + dy[k] * p;
-                                if (nx >= 0 && nx < N && ny >= 0 && ny < N && graph[nx][ny].equals("1")) {
-                                    cnt++;
-                                } else {
-                                    break;
-                                }
-                            }
-                            // 중간에 카운트 체크 상황 발생 시 반대 방향이 1인지 체크
-                            // ex 1 1 1 1 일 때, 두 번째 1부터 오른쪽으로 카운트 한다면, 반대 방향이 1인지 체크해야함. (첫 번째도 1이므로 세칸이 아님)
-                            int reverseDX = x + dx[k] * -1;
-                            int reverseDY = y + dy[k] * -1;
-                            if (reverseDX >= 0 && reverseDX < N && reverseDY >= 0 && reverseDY < N){
-                                if (cnt == (K - 1) && !graph[reverseDX][reverseDY].equals("1")) {
-                                    result++;
-                                }
-                            } else {
-                                if (cnt == (K - 1)){
-                                    result++;
-                                }
-                            }
-
+                    if (graph[x][y].equals("1")) {
+                        cnt++;
+                    } else {
+                        if (cnt == K) {
+                            result++;
                         }
+                        cnt = 0;
                     }
                 }
+                if (cnt == K) {
+                    result++;
+                }
             }
-            System.out.println("#" + t + " " + result / 2);
+
+            for (int y = 0; y < N; y++) {
+                int cnt = 0;
+                for (int x = 0; x < N; x++) {
+                    if (graph[x][y].equals("1")) {
+                        cnt++;
+                    } else {
+                        if (cnt == K) {
+                            result++;
+                        }
+                        cnt = 0;
+                    }
+                }
+                if (cnt == K) {
+                    result++;
+                }
+            }
+
+
+            System.out.println("#" + t + " " + result);
         }
     }
 }
