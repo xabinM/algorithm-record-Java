61479d94e737cb5e6a236709904fd3ed0afbd3c7
xabin <binine2@gmail.com>
2025-02-14 17:42:35 +0900

    algo : 단조 증가 임시

--- src/SSAFY/MonotoneIncreasing.java
+++ src/SSAFY/MonotoneIncreasing.java
@@ -1,7 +1,32 @@
 package SSAFY;
 
+import java.util.Scanner;
+
 public class MonotoneIncreasing {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int T = sc.nextInt();
+
+        for (int t = 1; t <= T; t++) {
+            int N = sc.nextInt();
+
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < N; i++) {
+                int max = 0;
+                for (int j = i; j < N; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        System.out.println("#" + t + " " + -1);
+
+                    } else {
+
+                    }
+                }
+            }
+        }
     }
 }
