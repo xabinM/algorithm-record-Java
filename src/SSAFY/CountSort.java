73a88fd8459fe25e2cb56b97596ed8af9bd22b3b
Min <binine2@gmail.com>
2025-02-16 18:05:11 +0900

    algo : 카운트 정렬 누적합 적용 o

--- src/SSAFY/CountSort.java
+++ src/SSAFY/CountSort.java
@@ -12,27 +12,33 @@ public class CountSort {
         for (int t = 1; t <= T; t++) {
             int N = sc.nextInt();;
 
+            // 원본 배열
             int[] arr = new int[N];
             for (int i = 0; i < N; i++) {
                 arr[i] = sc.nextInt();
             }
+            // 최대값
             int arrMaxValue = Arrays.stream(arr).max().orElseThrow();
 
+            // 카운트 배열
             int[] cnt = new int[arrMaxValue + 1];
             for (int i = 0; i < N; i++) {
                 cnt[arr[i]] += 1;
             }
             System.out.println(Arrays.toString(cnt));
 
+            //누적합
+            for (int i = 1; i <= arrMaxValue; i++) {
+                cnt[i] += cnt[i - 1];
+            }
+            System.out.println(Arrays.toString(cnt));
+
+            // 정렬
             int[] result = new int[N];
-            for (int i = 0; i < result.length; i++) {
-                for (int j = 0; j < cnt.length; j++) {
-                    if (cnt[j] != 0) {
-                        result[i] = j;
-                        cnt[j]--;
-                        break;
-                    }
-                }
+            for (int i = N - 1; i >= 0; i--) {
+                int value = arr[i];
+                result[cnt[value] - 1] = value;
+                cnt[value]--;
             }
 
             System.out.println(Arrays.toString(result));
