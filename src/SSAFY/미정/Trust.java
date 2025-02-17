1edf6a6e9b91b457b3656bb37fde9528cc71d34a
Min <binine2@gmail.com>
2025-02-17 20:28:48 +0900

    algo : 신뢰

--- src/SSAFY/미정/Trust.java
+++ src/SSAFY/미정/Trust.java
@@ -27,6 +27,7 @@ public class Trust {
             int oPos = 1;
             int bPos = 1;
 
+            // B 2 O 1 O 2 B 4
             for (Command command : commands) {
                 if (command.robotName.equals("O")) {
                     oTime += Math.abs(command.position - oPos) + 1;   // +1은 버튼 누르는 시간
@@ -36,18 +37,19 @@ public class Trust {
                         time++;
                         oTime = time;
                     }
-
-
+                    oPos = command.position;
                 } else {
-
-
-
-
-
+                    bTime += Math.abs(command.position - bPos) + 1;   // +1은 버튼 누르는 시간
+                    if (bTime > time) {
+                        time = bTime;
+                    } else {
+                        time++;
+                        bTime = time;
+                    }
+                    bPos = command.position;
                 }
             }
-
-
+            System.out.println("#" + t + " " + time);
         }
     }
 }
