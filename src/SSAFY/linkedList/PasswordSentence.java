8e2ed15d41024ce711f4db20981f29285d224fa2
xabinM <binine2@gmail.com>
2025-02-18 18:00:45 +0900

    algo : 암호문

---
+++ src/SSAFY/linkedList/PasswordSentence.java
@@ -0,0 +1,17 @@
+package SSAFY.linkedList;
+
+import java.util.Scanner;
+
+public class PasswordSentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int t = 1; t <= 10; t++) {
+            int N = sc.nextInt();
+            sc.nextLine();
+
+            String[] arr = sc.nextLine().split(" ");
+
+        }
+    }
+}
