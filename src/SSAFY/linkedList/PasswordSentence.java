8ce054b550bbb9b6e6f9e30024619bdad8518cca
Min <binine2@gmail.com>
2025-02-18 20:49:59 +0900

    algo : 암호1

--- src/SSAFY/linkedList/PasswordSentence.java
+++ src/SSAFY/linkedList/PasswordSentence.java
@@ -1,5 +1,6 @@
 package SSAFY.linkedList;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class PasswordSentence {
@@ -10,8 +11,38 @@ public class PasswordSentence {
             int N = sc.nextInt();
             sc.nextLine();
 
-            String[] arr = sc.nextLine().split(" ");
+            String[] origin = sc.nextLine().split(" ");
 
+            int M = sc.nextInt();
+            sc.nextLine();
+
+            String[] orders = sc.nextLine().split(" ");
+            int idx = 0;
+
+            LinkedList<String> result = new LinkedList<>();
+            while (idx < orders.length) {
+                if (orders[idx].equals("I")) {
+                    int insertPos = Integer.parseInt(orders[idx + 1]);
+                    int insertNum = Integer.parseInt(orders[idx + 2]);
+
+                    for (int i = 0; i < N; i++) {
+                        if (i == insertPos) {
+                            int idx2 = 0;
+                            for (int j = (idx + 3); j < (idx + insertNum + 3); j++) {
+                                result.add(insertPos + idx2, orders[j]);
+                                idx2++;
+                            }
+                        }
+                        result.add(origin[i]);
+                    }
+                    idx += 3 + insertNum;
+                }
+            }
+            System.out.print("#" + t + " ");
+            for (int i = 0; i < 10; i++) {
+                System.out.print(result.get(i) + " ");
+            }
+            System.out.println();
         }
     }
 }
