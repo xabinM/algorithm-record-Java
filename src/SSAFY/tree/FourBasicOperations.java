a2d5673af19b4068894a15504b99e080ce975317
Min <binine2@gmail.com>
2025-02-20 21:37:55 +0900

    임시 : 사칙연산 풀어야해요

---
+++ src/SSAFY/tree/FourBasicOperations.java
@@ -0,0 +1,15 @@
+package SSAFY.tree;
+
+import java.util.Scanner;
+
+public class FourBasicOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int t = 1; t <= 10; t++) {
+            int N = sc.nextInt();
+
+
+        }
+    }
+}
--- src/SSAFY/tree/SubTree.java
+++ src/SSAFY/tree/SubTree.java
@@ -15,7 +15,6 @@ public class SubTree {
             int N = sc.nextInt();
 
             int[] childArr = new int[E + 2];
-//            int[] parentArr = new int[E + 2];
 
             for (int i = 0; i < E; i++) {
                 int parent = sc.nextInt();
@@ -37,7 +36,6 @@ public class SubTree {
         }
     }
 
-
     private static void count(int[] childArr, int cur, int idx) {
         if (cur != 0) {
             cur = idx;
@@ -50,3 +48,17 @@ public class SubTree {
         }
     }
 }
+
+//    private void inorder()
+//    {
+//        //탈출시 값읗 넣어주고
+//        if()
+//        {
+//            9/6-4
+//        }0
+//        inorder();
+//        //
+//        inorder();
+//        //계산을 해준다. ( 스택에서 2만꺼내서
+//
+//    }
