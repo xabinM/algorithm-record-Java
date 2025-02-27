3adc5de9c16dfec2d154a4d91e453246b754c51d
Min <binine2@gmail.com>
2025-02-27 20:47:57 +0900

    algo : 벽돌 깨기 벽돌 폭파까지는 함

--- src/SSAFY/삼성A형/BreakBrick.java
+++ src/SSAFY/삼성A형/BreakBrick.java
@@ -36,28 +36,49 @@ public class BreakBrick {
         if (depth == N) {
             return min;
         }
-
-        for (int i = 0; i < N; i++) {
+        // 열을 순차적으로 돔 (y 좌표가 주어짐)
+        for (int y = 0; y < W; y++) {
             int[][] nGraph = new int[H][W];
             for (int j = 0; j < H; j++){
                 System.arraycopy(graph[j], 0, nGraph[j], 0, W);
             }
 
+            // 주어진 y 좌표가지고 맨 위에 있는 x 좌표를 찾아야지
+            int x = findX(nGraph, y);
+
+            if (x == 0) {
+                continue;
+            }
+
+            boom(nGraph, x, y);
 
         }
+        return 0;
+    }
+
+    // 그래프에서 맨 위에 있는 x좌표 찾는 메서드
+    private static int findX(int[][] graph, int y) {
+        for (int i = 0; i < H; i++) {
+            if (graph[i][y] != 0) {
+                return i;
+            }
+        }
+        return 0;
     }
 
     private static int[][] boom(int[][] graph, int x, int y) {
         int spread = graph[x][y];
+        graph[x][y] = 0;
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < spread; j++){
-                int nx = x + dx[i] * j;
-                int ny = y + dy[i] * j;
+        if (spread > 1){
+            for (int i = 0; i < 4; i++) {
+                for (int j = 1; j < spread; j++) {
+                    int nx = x + dx[i] * j;
+                    int ny = y + dy[i] * j;
 
-                if (nx >= 0 && nx < W && ny >= 0 && ny < H) {
-                    graph[x][y] = 0;
-                    boom(graph, nx, ny);
+                    if (nx >= 0 && nx < W && ny >= 0 && ny < H && graph[nx][ny] != 0) {
+                        boom(graph, nx, ny);
+                    }
                 }
             }
         }
