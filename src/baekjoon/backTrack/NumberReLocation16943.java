80b47997704908fbc47eea74eb31097de9d6e251
xabinM <binine2@gmail.com>
2025-03-12 18:00:35 +0900

    algo : 숫자 재배치 미완성

--- src/baekjoon/backTrack/NumberReLocation16943.java
+++ src/baekjoon/backTrack/NumberReLocation16943.java
@@ -1,12 +1,15 @@
 package baekjoon.backTrack;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NumberReLocation16943 {
     private static int A;
     private static int B;
-    private static String[] arr;
-    private static int len;
+    private static int[] arrA;
+    private static int[] arrB;
+    private static int lenA;
+    private static int lenB;
     private static boolean[] visited;
     private static int result;
 
@@ -15,20 +18,61 @@ public class NumberReLocation16943 {
 
         A = sc.nextInt();
         B = sc.nextInt();
-        arr = String.valueOf(A).split("");
-        len = arr.length;
-        visited = new boolean[len];
-        result = 0;
+        arrA = Arrays.stream(String.valueOf(A).split("")).mapToInt(Integer::parseInt).toArray();
+        arrB = Arrays.stream(String.valueOf(B).split("")).mapToInt(Integer::parseInt).toArray();
+        System.out.println("arrA : " + Arrays.toString(arrA));
+        System.out.println("arrB : " + Arrays.toString(arrB));
+        lenA = arrA.length;
+        lenB = arrB.length;
+        visited = new boolean[lenA];
+        result = Integer.MIN_VALUE;
+
+        int[] temp = new int[lenA];
 
 
+        if (lenA > lenB) {
+            System.out.println(-1);
+            return;
+        } else {
+            backTrack(temp, 0, false);
+        }
 
+        System.out.println(result);
     }
     // A 랑 B의 자리수를 비교해서 분기를 3개로 쪼개야될듯?
     // 1. A.len > B.len : B보다 작아질 수 있는 수가 없기 때문에 -1 출력
     // 2. A.len = B.len : A 바꿔가면서 찾아내
     // 3. A.len < B.len : A 로 만들 수 있는 수중에 가장 큰 수가 답
 
-    private static void backTrack(int[] temp, int depth) {
+    private static void backTrack(int[] temp, int depth, boolean alreadyBigger) {
+        if (depth == lenA) {
+            // 완성된 배열 하나의 숫자로 합치기
+            int cur = joinArr(temp);
 
+            // B보다 작으면서 최대 값 갱신
+            if (cur < B) {
+                result = Math.max(result, cur);
+            }
+            return;
+        }
+
+        for (int i = 0; i < lenA; i++) {
+
+            if (!visited[i]) {
+
+            }
+        }
+    }
+
+    private static int joinArr (int[] temp){
+        int cur = 0;
+        int location = temp.length;
+        for (int i = 0; i < temp.length; i++) {
+            if (location == 0) {
+                cur += temp[temp.length - 1];
+            }
+            cur += (int) (temp[i] * Math.pow(10, location-- - 1));
+        }
+        return cur;
     }
 }
