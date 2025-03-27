c22b6f499e0045e168081d2451b5d4c3004209eb
xabinM <binine2@gmail.com>
2025-03-27 09:40:40 +0900

    algo : 파ã핑 파핑 지뢰찾기 bfs

--- src/baekjoon/bfs/PoppingPopping.java
+++ src/baekjoon/bfs/PoppingPopping.java
@@ -1,6 +1,5 @@
 package baekjoon.bfs;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -11,7 +10,6 @@ public class PoppingPopping {
     private static final int[] dx = {-1, 1, 0, 0, 1, 1, -1, -1};
     private static final int[] dy = {0, 0, -1, 1, 1, -1, 1, -1};
     private static boolean[][] visited;
-    private static int cnt;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -26,65 +24,77 @@ public class PoppingPopping {
             for (int i = 0; i < N; i++) {
                 graph[i] = sc.nextLine().toCharArray();
             }
-        }
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(Arrays.toString(graph[i]));
-        }
-        System.out.println();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (graph[i][j] == '.') {
-                    graph[i][j] = '0';
-                    for (int k = 0; k < 8; k++) {
-                        int nx = i + dx[k];
-                        int ny = j + dy[k];
-                        if (nx >= 0 && nx < N && ny >= 0 && ny < N && graph[nx][ny] == '*') {
-                            graph[i][j] = '1';
-                            break;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (graph[i][j] == '.') {
+                        graph[i][j] = '0';
+                        for (int k = 0; k < 8; k++) {
+                            int nx = i + dx[k];
+                            int ny = j + dy[k];
+                            if (nx >= 0 && nx < N && ny >= 0 && ny < N && graph[nx][ny] == '*') {
+                                graph[i][j] = '1';
+                                break;
+                            }
                         }
                     }
                 }
             }
-        }
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(Arrays.toString(graph[i]));
-        }
-        System.out.println();
+            visited = new boolean[N][N];
+            int cnt = 0;
 
-        visited = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && graph[i][j] == '0') {
-                    cnt++;
-                    bfs(i, j);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!visited[i][j] && graph[i][j] == '0') {
+                        cnt++;
+                        bfs(new Node(i, j));
+                    }
                 }
             }
-        }
 
-        System.out.println("cnt : " + cnt);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!visited[i][j] && graph[i][j] == '1') {
+                        cnt++;
+                    }
+                }
+            }
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(Arrays.toString(visited[i]));
+            System.out.println("#" + t + " " + cnt);
         }
     }
 
-    private static void bfs(int r, int c) {
-        Queue<int[]> q = new LinkedList<>();
-        visited[r][c] = true;
-        q.offer(new int[]{r, c});
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0], y = cur[1];
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private static void bfs(Node start) {
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+
+            int x = cur.x;
+            int y = cur.y;
+            visited[x][y] = true;
+
             for (int i = 0; i < 8; i++) {
-                int nx = x + dx[i], ny = y + dy[i];
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny])
-                    continue;
-                visited[nx][ny] = true;
-                if (graph[nx][ny] == '0')
-                    q.offer(new int[]{nx, ny});
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    if (graph[nx][ny] == '0')
+                        queue.offer(new Node(nx, ny));
+                }
             }
         }
     }
