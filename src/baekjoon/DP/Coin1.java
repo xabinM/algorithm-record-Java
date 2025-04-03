3e9337cac430831e1c6e32ad8af361a34fc87c7d
xabinM <binine2@gmail.com>
2025-04-03 17:30:59 +0900

    algo : 동전2 dp

---
+++ src/baekjoon/DP/Coin1.java
@@ -0,0 +1,28 @@
+package baekjoon.DP;
+
+import java.util.Scanner;
+
+public class Coin1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        System.out.println(dp[k]);
+    }
+}
