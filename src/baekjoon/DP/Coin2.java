e25a32daaf32ba75a7c86ad05748930a8c0253a7
xabinM <binine2@gmail.com>
2025-04-03 17:59:03 +0900

    algo : 동전 2 dp 실패

---
+++ src/baekjoon/DP/Coin2.java
@@ -0,0 +1,34 @@
+package baekjoon.DP;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Coin2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        if (dp[k] == Integer.MAX_VALUE) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(dp[k]);
+    }
+}
