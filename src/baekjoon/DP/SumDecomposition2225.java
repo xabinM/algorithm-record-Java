e405c5d1a7ba98c2a2a25763b851235c3762f771
Min <binine2@gmail.com>
2025-04-06 20:42:39 +0900

    algo : 합분해 dp 사용

--- src/baekjoon/DP/SumDecomposition2225.java
+++ src/baekjoon/DP/SumDecomposition2225.java
@@ -2,7 +2,7 @@ package baekjoon.DP;
 
 import java.util.Scanner;
 
-public class SumDecomposition {
+public class SumDecomposition2225 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(); // 목표 합
