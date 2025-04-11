6f3b2bfb62ad0f6c1217c4897bc70da9ab6190b1
xabinM <binine2@gmail.com>
2025-04-12 00:57:01 +0900

    rename and algo : 유기농 배추 vfs

--- src/baekjoon/graphSearch/OrganicCabbage1012.java
+++ src/baekjoon/graphSearch/OrganicCabbage1012.java
@@ -1,38 +1,45 @@
 package baekjoon.graphSearch;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class OrganicCabbage {
+public class OrganicCabbage1012 {
     private static final int[] dx = {-1, 1, 0, 0};
     private static final int[] dy = {0, 0, -1, 1};
     private static int N, M;
     private static int[][] graph;
     private static boolean[][] visited;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
-            M = sc.nextInt();   // 가로
-            N = sc.nextInt();   // 세로
-            int K = sc.nextInt();   // 배추 개수
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
 
             graph = new int[N][M];
+            visited = new boolean[N][M];
+
             for (int i = 0; i < K; i++) {
-                int y = sc.nextInt();
-                int x = sc.nextInt();
+                st = new StringTokenizer(br.readLine());
+                int y = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
                 graph[x][y] = 1;
             }
-            visited = new boolean[N][M];
-            int cnt = 0;
 
+            int cnt = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    if (graph[i][j] == 1 && !visited[i][j]){
+                    if (graph[i][j] == 1 && !visited[i][j]) {
                         bfs(new Node(i, j));
                         cnt++;
                     }
@@ -45,12 +52,12 @@ public class OrganicCabbage {
     private static void bfs(Node node) {
         Queue<Node> queue = new ArrayDeque<>();
         queue.offer(node);
+        visited[node.x][node.y] = true;
 
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
             int x = cur.x;
             int y = cur.y;
-            visited[x][y] = true;
 
             for (int k = 0; k < 4; k++) {
                 int nx = x + dx[k];
