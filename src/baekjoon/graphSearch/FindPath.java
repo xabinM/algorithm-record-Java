1f6b6071cd71c8a12d58ca8bea9c9d3623a8a921
xabinM <binine2@gmail.com>
2025-04-15 19:15:06 +0900

    경로찾기 플루이드 워샬

---
+++ src/baekjoon/graphSearch/FindPath.java
@@ -0,0 +1,38 @@
+package baekjoon.graphSearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindPath {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        int[][] graph = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            graph[i] = Arrays.stream(sc.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
