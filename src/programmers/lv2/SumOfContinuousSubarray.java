32fd540b00558865ee5aba87d527c5cd3c3bb7f4
xabinM <binine2@gmail.com>
2026-01-19 20:30:26 +0900

    programmers: 연속된 부분 수열의 합 sliding windows

--- src/programmers/lv2/SumOfContinuousSubarray.java
+++ src/programmers/lv2/SumOfContinuousSubarray.java
@@ -17,61 +17,27 @@ public class SumOfContinuousSubarray {
     }
 
     public static int[] solution(int[] sequence, int k) {
-        LinkedList<int[]> result = new LinkedList<>();
+        int left = 0, right = 0;
+        int sum = sequence[0];
+        int bestL = 0, bestR = Integer.MAX_VALUE;
 
-        for (int i = 0; i < sequence.length; i++) {
-            int[] temp = subarray(i, sequence, k);
-
-            if (temp != null) {
-                result.add(temp);
-            }
-        }
-
-        if (result.size() == 1) {
-            return result.getFirst();
-        } else {
-            return result.get(check(result));
-        }
-    }
-
-    private static int check(List<int[]> result) {
-        int idx = 0;
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < result.size(); i++) {
-            int[] r = result.get(i);
-            int temp = Math.min((r[1] - r[0]), min);
-
-            if (min != temp) {
-                idx = i;
-                min = temp;
-            }
-        }
-
-        return idx;
-    }
-
-    private static int[] subarray(int idx, int[] sequence, int k) {
-
-        int[] result = new int[2];
-        int sum = 0;
-        boolean flag = false;
-
-        for (int i = idx; i < sequence.length; i++) {
-            sum += sequence[i];
-
+        while (left <= right && right < sequence.length) {
             if (sum == k) {
-                result[0] = idx;
-                result[1] = i;
-                flag = true;
-                break;
+                if ((bestR - bestL) > (right - left)) {
+                    bestL = left;
+                    bestR = right;
+                }
+                sum -= sequence[left++];
+            } else if (sum < k) {
+                right++;
+                if (right < sequence.length) {
+                    sum += sequence[right];
+                }
+            } else {
+                sum -= sequence[left++];
             }
         }
 
-        if (flag) {
-            return result;
-        } else {
-            return null;
-        }
+        return new int[]{bestL, bestR};
     }
 }
