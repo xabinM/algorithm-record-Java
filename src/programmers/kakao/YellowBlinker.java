06f40bad07128c61880fedd7d9636fa7b1902264
xabinM <binine2@gmail.com>
2026-03-15 12:17:55 +0900

    programmers: 노란불 신호등 구현 (범위 설정 전)

--- src/programmers/kakao/YellowBlinker.java
+++ src/programmers/kakao/YellowBlinker.java
@@ -1,4 +1,44 @@
 package programmers.kakao;
 
 public class YellowBlinker {
+
+    public int solution(int[][] signals) {
+
+        int G1 = signals[0][0];
+        int Y1 = signals[0][1];
+        int R1 = signals[0][2];
+
+        int C1 = G1 + Y1 + R1;
+
+        for (int k = 0; k < 100000; k++) {
+
+            for (int o = 0; o < Y1; o++) {
+
+                int t = G1 + o + k * C1;
+
+                boolean ok = true;
+
+                for (int i = 1; i < signals.length; i++) {
+
+                    int G =  signals[i][0];
+                    int Y =  signals[i][1];
+                    int R = signals[i][2];
+
+                    int C = G + Y + R;
+
+                    int pos = t % C;
+
+                    if (pos < G || pos >= G + Y) {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if (ok) {
+                    return t + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
