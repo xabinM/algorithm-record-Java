939398ab1fb0f99840d8b4d6d7c1a6af883860ca
xabinM <binine2@gmail.com>
2026-03-20 22:24:43 +0900

    programmers: 네트워크 dfs

--- src/programmers/lv3/Network.java
+++ src/programmers/lv3/Network.java
@@ -1,4 +1,30 @@
 package programmers.lv3;
 
 public class Network {
+
+    public static int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, computers);
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    private static void dfs(int node,  boolean[] visited, int[][] computers) {
+        visited[node] = true;
+
+        for (int i = 0; i < computers.length; i++) {
+
+            if (computers[node][i] == 1 && !visited[i]) {
+                dfs(i,  visited, computers);
+            }
+        }
+    }
 }
